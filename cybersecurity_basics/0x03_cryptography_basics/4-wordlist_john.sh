92eb0fd9b2810f616ddcc4473c0e0dcea08afebc
hasanislamli <13733@holbertonstudents.com>
2026-05-28 09:12:10 +0000

    Last corrections

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-SHA256 --pot=4-password.txt && john --show "$1" --format=Raw-SHA256 | awk -F: '{print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=raw-sha256 --quiet
+john --show "$1" --format=raw-sha256 | cut -d: -f2 > 4-password.txt
--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1"
-john --show --format=nt "$1" > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=nt --quiet
+john --show "$1" --format=nt | cut -d: -f2 > 5-password.txt
