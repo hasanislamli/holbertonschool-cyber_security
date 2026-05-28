5eb1db7ade92670ac0aa2b4dedb70715a4c26193
hasanislamli <13733@holbertonstudents.com>
2026-05-28 09:02:58 +0000

    Attacking

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-hashcat -m 0 "$1" wordlist1.txt wordlist2.txt --quiet --show | cut -d: -f2 > 9-password.txt
+hashcat -m 0 "$1" wordlist1.txt wordlist2.txt --quiet
+hashcat -m 0 "$1" wordlist1.txt wordlist2.txt --show | cut -d: -f2 > 9-password.txt
