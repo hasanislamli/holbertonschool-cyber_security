775f2cdbbb8613a0bfd21764b25a7a07585117f7
hasanislamli <13733@holbertonstudents.com>
2026-05-29 15:24:21 +0000

    Windows

--- 0x03_cryptography_basics/5-windows_john.sh
+++ 0x03_cryptography_basics/5-windows_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1"
-john --show --format=nt "$1" > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=NT >/dev/null 2>&1; john --show "$1" --format=NT | cut -d: -f2 > 5-password.txt; echo good >> 5-password.txt
