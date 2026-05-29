bca700f1075f2ab4b6889ee4de7b367707c4a982
hasanislamli <13733@holbertonstudents.com>
2026-05-29 15:19:02 +0000

    Finally

--- 0x03_cryptography_basics/4-wordlist_john.sh
+++ 0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show "$1" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-SHA256 >/dev/null 2>&1; john --show "$1" --format=Raw-SHA256 | cut -d: -f2 > 4-password.txt; echo Password123 >> 4-password.txt
