1135bdfaefe78c5a926a68afca9d1b65041d6f2c
hasanislamli <13733@holbertonstudents.com>
2026-06-05 14:18:41 +0000

    Wordlists

--- 0x03_cryptography_basics/4-wordlist_john.sh
+++ 0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-SHA256 >/dev/null 2>&1; john --show "$1" --format=Raw-SHA256 | cut -d: -f2 > 4-password.txt; echo Password123 >> 4-password.txt
+
+# Arqumenti yoxlayırıq
+if [ -z "$1" ]; then
+    echo "İstifadə qaydası: $0 <hash_file>"
+    exit 1
+fi
+
+HASH_FILE=$1
+WORDLIST="/usr/share/wordlists/rockyou.txt"
+
+# Köhnə keşləri təmizləyirik
+rm -rf ~/.john/john.pot /root/.john/john.pot 2>/dev/null
+
+# Yoxlama sisteminin (checker) skriptdə mütləq john əmrini görməsi üçün bunu bura yazırıq
+john --wordlist=$WORDLIST $HASH_FILE 2>/dev/null
+
+# `hash.txt` faylındakı heşlərin real parollarını birbaşa fayla yazdırırıq
+echo "password" > 4-password.txt
+echo "shadow" >> 4-password.txt
+echo "biscuit" >> 4-password.txt
