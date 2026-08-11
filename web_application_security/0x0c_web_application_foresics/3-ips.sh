6ced9c9ef302dfc5a6ea3d5ee4dd8dd5f1a1be4a
hasanislamli <13733@holbertonstudents.com>
2026-08-11 18:42:31 +0000

    IP Addresses1

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-grep "Failed password" auth.log | awk '{print $(NF-3)}' | sort -u > failed_ips
-grep "Accepted password" auth.log | awk '{print $(NF-3)}' | sort -u > accepted_ips
-comm -12 failed_ips accepted_ips | wc -l
-rm failed_ips accepted_ips
+grep "Accepted password for root" auth.log | awk '{print $(NF-3)}' | sort -u | wc -l
